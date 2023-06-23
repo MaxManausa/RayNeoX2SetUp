b332bf0ede53ba2e04abae8e72db413e7922cdd9
Max-Manausa <maxmanausa@gmail.com>
2023-06-22 22:34:11 -0400

    Made a working bow and arrow plus working moving targets

---
+++ Assets/Scripts/FreezeOnCollision.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeOnCollision : MonoBehaviour
+{
+    private Rigidbody arrowRigidbody;
+    private void Start()
+    {
+        arrowRigidbody = GetComponent<Rigidbody>();
+        arrowRigidbody.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.rigidbody != null && collision.rigidbody.CompareTag("Target"))
+        {
+            transform.SetParent(collision.transform);
+            arrowRigidbody.isKinematic = true; 
+            arrowRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+            Debug.Log("arrow collided with" + name);
+        }
+    }
+}
+
---
+++ Assets/Scripts/MovingTargetHit.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingTargetHit : MonoBehaviour
+{
+    [SerializeField] public Material hitMaterial;
+    [SerializeField] private TargetController _targetController;
+    //[SerializeField] private Scoreboard _scoreboard;
+
+    [SerializeField] public Material originalMaterial;
+    public Renderer renderer;
+
+    
+    [SerializeField] private GameObject winScreen;
+    
+    [SerializeField] private AudioSource arrowAudioSource;
+    [SerializeField] private AudioClip arrowHitSound;
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        renderer.material = originalMaterial;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Arrow"))
+        {
+            renderer.material = hitMaterial;
+            if (_targetController.enabled == true)
+            {
+                //_scoreboard.score += 1;
+                //PlaySound(arrowHitSound);
+            }
+            /*
+            if (_scoreboard.score > 9)
+            
+            {
+                winScreen.SetActive(true);
+                Debug.Log("It happened!");
+            }
+            */
+            _targetController.enabled = false;
+            //Debug.Log(_scoreboard.score);
+            Invoke("TargetBackOn",6f);
+        }
+    }
+
+    void TargetBackOn()
+    {
+        //turns the target back on
+        renderer.material = originalMaterial;
+        _targetController.enabled = true;
+    }
+
+    /*
+    private void PlaySound(AudioClip newSound)
+    {
+        newSound = arrowHitSound;
+        arrowAudioSource.Play();
+    }
+    */
+}
+
